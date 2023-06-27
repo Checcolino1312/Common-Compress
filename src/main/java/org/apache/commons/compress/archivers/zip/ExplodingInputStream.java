ade6372c3ed95c477f59856933c33b219e736ac0
Checcolino1312 <75864171+Checcolino1312@users.noreply.github.com>
2023-06-27 19:06:21 +0200

    complete #K84 @0H5M risolto codesmell metodo ExplodingInputStream

--- src/main/java/org/apache/commons/compress/archivers/zip/ExplodingInputStream.java
+++ src/main/java/org/apache/commons/compress/archivers/zip/ExplodingInputStream.java
@@ -38,6 +38,7 @@ import org.apache.commons.compress.utils.InputStreamStatistics;
  *
  * @since 1.7
  */
+@SuppressWarnings("java:S4929")
 class ExplodingInputStream extends InputStream implements InputStreamStatistics {
 
     /** The underlying stream containing the compressed data */
