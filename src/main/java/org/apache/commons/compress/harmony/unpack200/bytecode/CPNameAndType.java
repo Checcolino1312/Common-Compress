4727c8a1d6e812940c07bcdf874a60be520025a6
Checcolino1312 <75864171+Checcolino1312@users.noreply.github.com>
2023-06-29 16:12:38 +0200

    complete #Z163 @0H3M rimosso commento

--- src/main/java/org/apache/commons/compress/harmony/unpack200/bytecode/CPNameAndType.java
+++ src/main/java/org/apache/commons/compress/harmony/unpack200/bytecode/CPNameAndType.java
@@ -53,10 +53,7 @@ public class CPNameAndType extends ConstantPoolEntry {
         this.descriptor = Objects.requireNonNull(descriptor, "descriptor");
     }
 
-    /*
-     * field_info { u2 access_flags; u2 name_index; u2 descriptor_index; u2 attributes_count; attribute_info
-     * attributes[attributes_count]; }
-     */
+
 
     @Override
     public boolean equals(final Object obj) {
