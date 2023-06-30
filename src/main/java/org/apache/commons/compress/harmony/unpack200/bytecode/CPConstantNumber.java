f6e8fed193d8a1bcd8c0d820ad22babef68d2593
MatteoKR <mat.razionale@gmail.com>
2023-06-30 13:34:09 +0200

    complete #Z41 @0h2m Cambiata la visibilità del costruttore a "protected".

--- src/main/java/org/apache/commons/compress/harmony/unpack200/bytecode/CPConstantNumber.java
+++ src/main/java/org/apache/commons/compress/harmony/unpack200/bytecode/CPConstantNumber.java
@@ -21,7 +21,7 @@ package org.apache.commons.compress.harmony.unpack200.bytecode;
  */
 public abstract class CPConstantNumber extends CPConstant {
 
-    public CPConstantNumber(final byte tag, final Object value, final int globalIndex) {
+    protected CPConstantNumber(final byte tag, final Object value, final int globalIndex) {
         super(tag, value, globalIndex);
     }
 
