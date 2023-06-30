ae0531790369864865bf84c434d825df3a611543
MatteoKR <mat.razionale@gmail.com>
2023-06-30 13:40:00 +0200

    complete #Z49 @0h2m Cambiata la visibilità del costruttore a "protected".

--- src/main/java/org/apache/commons/compress/harmony/unpack200/bytecode/forms/VariableInstructionForm.java
+++ src/main/java/org/apache/commons/compress/harmony/unpack200/bytecode/forms/VariableInstructionForm.java
@@ -22,7 +22,7 @@ package org.apache.commons.compress.harmony.unpack200.bytecode.forms;
  */
 public abstract class VariableInstructionForm extends ByteCodeForm {
 
-    public VariableInstructionForm(final int opcode, final String name) {
+    protected VariableInstructionForm(final int opcode, final String name) {
         super(opcode, name);
     }
 
