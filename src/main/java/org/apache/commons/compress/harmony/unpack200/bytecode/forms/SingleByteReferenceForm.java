a35fb9d986afc12ac7c78a6bc8747739813629aa
MatteoKR <mat.razionale@gmail.com>
2023-06-30 13:38:38 +0200

    complete #Z47 @0h2m Cambiata la visibilità del costruttore a "protected".

--- src/main/java/org/apache/commons/compress/harmony/unpack200/bytecode/forms/SingleByteReferenceForm.java
+++ src/main/java/org/apache/commons/compress/harmony/unpack200/bytecode/forms/SingleByteReferenceForm.java
@@ -32,6 +32,7 @@ public abstract class SingleByteReferenceForm extends ReferenceForm {
         super(opcode, name, rewrite);
     }
 
+
     @Override
     protected abstract int getOffset(OperandManager operandManager);
 
