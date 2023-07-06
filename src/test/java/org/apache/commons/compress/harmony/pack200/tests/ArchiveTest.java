b3b12cb80ce10a49bc6620cc81902e1e2fb0f096
Checcolino1312 <75864171+Checcolino1312@users.noreply.github.com>
2023-07-06 16:19:52 +0200

    complete #J392 @0H1M Rimosso "public"

--- src/test/java/org/apache/commons/compress/harmony/pack200/tests/ArchiveTest.java
+++ src/test/java/org/apache/commons/compress/harmony/pack200/tests/ArchiveTest.java
@@ -194,7 +194,7 @@ import org.junit.jupiter.params.provider.MethodSource;
     }
 
     @Test
-    public void testHelloWorld() throws IOException, Pack200Exception, URISyntaxException {
+     void testHelloWorld() throws IOException, Pack200Exception, URISyntaxException {
         in = new JarFile(new File(Archive.class.getResource(
                 "/pack200/hw.jar").toURI()));
         file = File.createTempFile("helloworld", ".pack.gz");
