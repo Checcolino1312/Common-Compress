ef990d8e7794518e15b2a9591de96a1ee362b8a2
Checcolino1312 <75864171+Checcolino1312@users.noreply.github.com>
2023-07-06 10:51:04 +0200

    complete #J293 @0H1M Rimosso "public"

--- src/test/java/org/apache/commons/compress/harmony/unpack200/tests/bytecode/ByteCodeTest.java
+++ src/test/java/org/apache/commons/compress/harmony/unpack200/tests/bytecode/ByteCodeTest.java
@@ -25,7 +25,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-public class ByteCodeTest {
+ class ByteCodeTest {
 
     static Stream<Arguments> byteCode() {
         return Stream.of(
